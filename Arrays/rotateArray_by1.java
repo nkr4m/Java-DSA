45885d35bf937507d37587a26b935bdf69a6eae3
N.K Ramachandran <nnkrama11@gmail.com>
2020-12-22 18:23:07 +0530

    *

---
+++ Arrays/rotateArray_by1.java
@@ -0,0 +1,42 @@
+class array_rotate{
+	public void rotate_by1(int arr[], int size) {
+		
+		int temp = arr[0];
+		for(int i=0; i<size-1; i++) {
+			arr[i] = arr[i+1];
+			
+		}
+		arr[size-1] = temp;
+	}
+	
+	public void print_array(int arr[], int size) {
+		
+		for(int k:arr) {
+			System.out.print(k + " ");
+		}
+	}
+	
+}
+
+
+public class rotateArray_by1 {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,4,5};
+		int size = arr.length;
+		
+		System.out.println("original array");
+		for(int k:arr) {
+			System.out.print(k + " ");
+		}
+		
+		System.out.println();
+		array_rotate rotate = new array_rotate();
+		rotate.rotate_by1(arr, size);
+		System.out.println("rotated array");
+		rotate.print_array(arr, size);
+
+	}
+
+}
