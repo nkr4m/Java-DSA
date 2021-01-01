8cd8f9c5eefb7be751bf958b983bee83f285a9d2
N.K Ramachandran <nnkrama11@gmail.com>
2021-01-01 20:05:56 +0530

    strings

---
+++ Strings/07. replaceChar2.java
@@ -0,0 +1,14 @@
+package String;
+
+public class replaceChar12 {
+
+	public static void main(String[] args) {
+		
+		String str = "aabbbcaaaa";
+		String ans = str.replaceAll("a", "");
+		
+		System.out.println(ans);
+
+	}
+
+}
